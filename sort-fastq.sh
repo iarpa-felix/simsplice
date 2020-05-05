5b243c34e597f08f44a065149b89d12ca215e2b5
Ben Booth <benwbooth@gmail.com>
2020-05-04 18:08:20 -0700

    fix scripts

--- sort-fastq.sh
+++ sort-fastq.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
-if [[ ${1##*.} = gz ]]; then gzip -cd "$1" else cat "$1" fi | \
+if [[ ${1##*.} = gz ]]; then gzip -cd "$1"; else cat "$1"; fi | \
 paste - - - - | \
 sort -k1,1 | \
 tr "\t" "\n" | \
-if [[ ${1##*.} = gz ]]; then gzip else cat fi \
+if [[ ${1##*.} = gz ]]; then pigz||gzip; else cat; fi \
 >"${2-$1}.$$.tmp"
-mv -v "${2-$1}.$$"{.tmp,}
+mv -v "${2-$1}"{.$$.tmp,}
