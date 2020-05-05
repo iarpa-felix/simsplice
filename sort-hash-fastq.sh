674330d5a66e687401b64691b6066012e85cb3cb
Ben Booth <benwbooth@gmail.com>
2020-05-04 17:55:46 -0700

    fix hash script

--- sort-hash-fastq.sh
+++ sort-hash-fastq.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 if [[ ${1##*.} = gz ]]; then gzip -cd "$1" else cat "$1" fi | \
+perl -MDigest::SHA=sha256_hex -ple 's/^([\@\+])(.+)$/$1.sha256_hex($2)/e' | \
 paste - - - - | \
 sort -k1,1 | \
 tr "\t" "\n" | \
-perl -MDigest::SHA=sha256_hex -ple 's/^([\@\+])(.+)$/$1.sha256_hex($2)/e' | \
 if [[ ${1##*.} = gz ]]; then gzip else cat fi \
 >"${2-$1}.$$.tmp"
 mv -v "${2-$1}.$$"{.tmp,}
