0fd3980caab5539f181581bac646d9892f6fb293
Ben Booth <benwbooth@gmail.com>
2020-07-07 13:26:37 -0700

    single or paired end support

--- shuffle-fastq.sh
+++ shuffle-fastq.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 in1=$1
-in2=$2
-out1=${3-$1}
-out2=${4-$2}
+out1=${2-$1}
+in2=${3-}
+out2=${4-${3-}}
 paste <(if [[ ${in1##*.} = gz ]]; then gzip -cd "$in1"; else cat "$in1"; fi |paste - - - -) \
-      <(if [[ ${in2##*.} = gz ]]; then gzip -cd "$in2"; else cat "$in2"; fi |paste - - - -) | \
+      <(if [[ -n ${in2-} ]] then if [[ ${in2##*.} = gz ]]; then gzip -cd "$in2"; else cat "$in2"; fi |paste - - - -; fi) | \
 shuf | \
-perl -F\\t -lane 'print for @F[0..3]; print STDERR for @F[4..7]' \
+perl -F\\t -lane 'print for @F[0..3]; print STDERR for @F[4..7] if @F >= 8' \
   > >(if [[ ${out1##*.} = gz ]]; then pigz||gzip; else cat; fi >"$out1.$$.tmp") \
-  2> >(if [[ ${out2##*.} = gz ]]; then pigz||gzip; else cat; fi >"$out2.$$.tmp")
+  2> >(if [[ -n ${out2-} ]]; then if [[ ${out2##*.} = gz ]]; then pigz||gzip; else cat; fi >"$out2.$$.tmp"; else cat >&2; fi)
 mv -v "$out1"{.$$.tmp,}
-mv -v "$out2"{.$$.tmp,}
+if [[ -n ${out2-} ]]; then mv -v "$out2"{.$$.tmp,}; fi
