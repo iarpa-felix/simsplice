d1f26a3e33520525d51517c7746f9ec76cfe9044
Ben Booth <benwbooth@gmail.com>
2020-07-08 14:38:50 -0700

    bug fixes

--- shuffle-fastq.sh
+++ shuffle-fastq.sh
@@ -3,6 +3,6 @@ set -eu -o pipefail
 in1=${1-/dev/stdin}
 in2=${2-}
 paste <(if [[ ${in1##*.} = gz ]]; then gzip -cd "$in1"; else cat "$in1"; fi |paste - - - -) \
-      <(if [[ -n ${in2-} ]] then if [[ ${in2##*.} = gz ]]; then gzip -cd "$in2"; else cat "$in2"; fi |paste - - - -; fi) | \
+      <(if [[ -n ${in2-} ]]; then if [[ ${in2##*.} = gz ]]; then gzip -cd "$in2"; else cat "$in2"; fi |paste - - - -; fi) | \
 shuf | \
-perl -F\\t -lane 'print for @F[0..3]; print STDERR for @F[4..7] if @F >= 8'
+perl -F\\t -lane 'print for @F[0..3]; if (@F >= 8) {print STDERR for @F[4..7]}'
