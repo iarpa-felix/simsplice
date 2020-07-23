b1e37108b1bf5e664c4903d8d3d35427d0908bf4
Ben Booth <benwbooth@gmail.com>
2020-07-23 14:05:44 -0700

    test for zero-length chains

--- comparative/vcf2chainbed.sh
+++ comparative/vcf2chainbed.sh
@@ -7,18 +7,18 @@ if (/^##contig=<([^>]+)>$/) {
 next if /^#/; 
 die if $F[4]=~/,/; 
 if ($F[0] ne $c) {
-  print join "\t",$c,$r,$r+($c{$c}-$q),"+",$c,$q,$c{$c},"+",1,0,0,$c{$c}-$q if defined $c; 
-  print STDERR join "\t",$c,$q,$c{$c},"+",$c,$r,$r+($c{$c}-$q),"+",1,0,0,$c{$c}-$q if defined $c; 
+  print join "\t",$c,$r,$r+($c{$c}-$q),"+",$c,$q,$c{$c},"+",1,0,0,$c{$c}-$q if defined $c && $c{$c}-$q > 0; 
+  print STDERR join "\t",$c,$q,$c{$c},"+",$c,$r,$r+($c{$c}-$q),"+",1,0,0,$c{$c}-$q if defined $c && $c{$c}-$q > 0; 
   $q=0; 
   $r=0; 
   $c=$F[0];
 } 
-print join "\t",$F[0],$r,$r+(($F[1]-1)-$q),"+",$F[0],$q,($F[1]-1),"+",1,0,0,(($F[1]-1)-$q); 
-print STDERR join "\t",$F[0],$q,($F[1]-1),"+",$F[0],$r,$r+(($F[1]-1)-$q),"+",1,0,0,(($F[1]-1)-$q); 
-$r+=(($F[1]-1)-$q)+length($F[4]); 
-$q=($F[1]-1)+length($F[3]); 
+print join "\t",$F[0],$r,$r+($F[1]-$q),"+",$F[0],$q,($F[1]),"+",1,0,0,$F[1]-$q if $F[1]-$q > 0; 
+print STDERR join "\t",$F[0],$q,$F[1],"+",$F[0],$r,$r+($F[1]-$q),"+",1,0,0,$F[1]-$q if $F[1]-$q > 0; 
+$r+=($F[1]-$q)+length($F[4]); 
+$q=$F[1]+length($F[3]); 
 END{
-  print join "\t",$c,$r,$r+($c{$c}-$q),"+",$c,$q,$c{$c},"+",1,0,0,$c{$c}-$q;
-  print STDERR join "\t",$c,$q,$c{$c},"+",$c,$r,$r+($c{$c}-$q),"+",1,0,0,$c{$c}-$q;
+  print join "\t",$c,$r,$r+($c{$c}-$q),"+",$c,$q,$c{$c},"+",1,0,0,$c{$c}-$q if $c{$c}-$q > 0;
+  print STDERR join "\t",$c,$q,$c{$c},"+",$c,$r,$r+($c{$c}-$q),"+",1,0,0,$c{$c}-$q if $c{$c}-$q > 0;
 }
 ' "$@"
