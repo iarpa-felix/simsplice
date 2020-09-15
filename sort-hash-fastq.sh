c469a0332c111a5e37e275bbd59fe77d052ca8ac
Ben Booth <benwbooth@gmail.com>
2020-09-15 15:21:32 -0700

    fix fastq transform

--- sort-hash-fastq.sh
+++ sort-hash-fastq.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 if [[ ${1##*.} = gz ]]; then gzip -cd "$1"; else cat "$1"; fi | \
-  perl -MDigest::SHA=sha256_hex -ple 'BEGIN{$s=shift} s/^(\@)(.+)$/$1.sha256_hex("$2.$s")/e; s/^\+.*$/+/' "${3-}" | \
+  perl -MDigest::SHA=sha256_hex -ple 'BEGIN{$s=shift} s/^(\@)(\S+).*$/$1.sha256_hex("$2.$s")/e if ($.-1) % 4 == 0; s/^\+.*$/+/ if ($.-1) % 4 == 2' "${3-}" | \
 paste - - - - | \
 sort -k1,1 | \
 tr "\t" "\n" | \
